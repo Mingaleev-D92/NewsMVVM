15790c03c4e94922393d84f264f0f0904a775d87
Mingaleev-D92 <mingaleev.d92@mail.ru>
2022-07-05 10:51:32 +0300

    настройка ДАО

--- app/src/main/java/com/example/newsmvvm/pojo/Article.kt
+++ app/src/main/java/com/example/newsmvvm/pojo/Article.kt
@@ -1,6 +1,12 @@
 package com.example.newsmvvm.pojo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String? = null,
     val content: String = "",
     val description: String? = null,
